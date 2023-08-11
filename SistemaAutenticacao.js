0e476c648cb23952ed5ff1fd55bbcde42763bccb
Luís Campanholi <lpcampanholi@gmail.com>
2023-08-11 09:41:54 -0300

    arquivos

---
+++ SistemaAutenticacao.js
@@ -0,0 +1,20 @@
+/*
+Ser autenticável significa ter a propriedade "senha"
+duck type
+*/
+
+export class SistemaAutenticacao {
+  static login(autenticavel, senha) {
+    if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    return (
+      "autenticar" in autenticavel &&
+      autenticavel.autenticar instanceof Function
+    );
+  }
+}
